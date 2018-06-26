e254522acb435876cbf6bfe7cc9e2d0241e6e3f6
Cheng Aihua <chengah@zonekey.com.cn>
2018-06-26 15:48:51 +0800

    Navigator初始化

--- index.android.js
+++ index.android.js
@@ -4,50 +4,29 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
 } from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components'
+import WebViewTest from './modules/WebViewTest'
 
 export default class PracticeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    renderScene(route, navigator) {
+        let Component = route.component;
+        return <Component navigator={navigator} {...route.params}/>
+    }
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{component: WebViewTest}}
+                renderScene={(route, navigator) => this.renderScene(route, navigator)}
+            />
+        )
+    }
+
+}
 
 AppRegistry.registerComponent('PracticeProject', () => PracticeProject);
